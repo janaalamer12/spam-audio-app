bfa42b1115174cb4d860af6e2ddca02b22836e77
janaalamer12 <janaalamer2016@gmail.com>
2025-05-03 04:25:27 +0300

    Update app_spam.py

--- app_spam.py
+++ app_spam.py
@@ -7,6 +7,8 @@ import streamlit as st
 import whisper
 from nltk.corpus import stopwords
 
+# Download stopwords
+nltk.download('stopwords')
 
 # Load Whisper model
 asr_model = whisper.load_model("base")
